ae8d31811b3d7423567b5f722d52f594cd55ba52
jisonZ <haochenz@umich.edu>
2024-02-02 11:00:59 -0600

    tested on port3000, configured CORS

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,12 @@ from app.errors.handlers import errors
 from app.home.routes import home
 from app.utils.routes import utils
 from app.components.routes import components, component_init
+from app.modelflow.routes import model 
 
 # import db and models
 from .extensions import db
 from .entities.models import *
+from flask_cors import CORS, cross_origin
 
 # app = Flask(__name__)
 # app.config.from_object(DevelopmentConfig)
@@ -21,6 +23,8 @@ from .entities.models import *
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_object(DevelopmentConfig)
 
     # app.config.from_object(DevelopmentConfig if os.environ.get(
@@ -30,6 +34,7 @@ def create_app():
     app.register_blueprint(home)
     app.register_blueprint(utils)
     app.register_blueprint(components)
+    app.register_blueprint(model)
 
     # initialize
     component_init()
--- app/modelflow/routes.py
+++ app/modelflow/routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, render_template, request
 from flask import current_app as app
 
-modelflow = Blueprint('modelflow', __name__)
+model = Blueprint('model', __name__)
 
-@modelflow.route('/api/v1/chatflows', methods=['POST', 'GET'])
+@model.route('/api/v1/chatflows', methods=['POST', 'GET'])
 def save_chatflow():
   '''
   save chatflow
   '''
   if request.method == 'POST':
-    print("post chatflow")
+    app.logger.info(request)
 
   if request.method == 'GET':
-    print("get chatflow")
+    app.logger.info(request)
+    return "chatflow", 200
